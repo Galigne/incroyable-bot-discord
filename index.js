38456b25a9419c8eb26ee3ac94efdc2ba7101f27
Galigne <joel.galigne@gmail.com>
2021-04-17 12:04:00 +0200

    poutouyemoun

--- index.js
+++ index.js
@@ -10,10 +10,21 @@ const {
 	unIncroyableBot,
 	lePlusIncroyable,
 	cmd,
+	elite,
+	elite_voice,
 	chat,
 	replays,
 	musiques,
-	elite
+	moba_voice,
+	fps_voice,
+	rpg_voice,
+	fighting_voice,
+	souls_voice,
+	br_voice,
+	jdr_voice,
+	bar_voice,
+	equipe,
+	equipe_voice
 } = require('./config.json');
 
 const client = new Client();
@@ -55,14 +66,14 @@ client.on('message', async message => {
 				message.reply('Tu n\'es pas assez incroyable pour utiliser cette commande');
 			}
 		} else if (commandName == "play" || commandName == "skip" || commandName == "stop"){
-			if (message.channel.id === une_incroyable_cmd || message.channel.id === des_incroyables_musiques){
+			if (message.channel.id === cmd || message.channel.id === musiques){
 				command.execute(message);
 			} else {
 				message.reply('Cette commande doit etre utilisée dans le channel \"des_incroyables_musiques\"');
 				message.delete();
 			}
 		} else if (commandName == "restart") {
-			if (message.channel.id === une_incroyable_cmd && message.member.roles.cache.some(role => role.id === lePlusIncroyable)){
+			if (message.channel.id === cmd && message.member.roles.cache.some(role => role.id === lePlusIncroyable)){
 				command.execute(message, client, token);
 			} else {
 				message.reply('Vous n\'avait pas accès à cette commande');
@@ -95,5 +106,14 @@ client.on('guildMemberAdd', guildMember =>{
 	}
 });
 
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+	let newUserChannel = newMember.channel;
+
+	if(newUserChannel !== null && newUserChannel.id === equipe_voice && oldMember.member.id !== unIncroyableBot) {
+		newMember.member.voice.channel.join().then(VoiceConnection => {
+			VoiceConnection.play("./music/Poutouyemoun.mp3").on("finish", () => VoiceConnection.disconnect());
+		}).catch(e => console.log(e))
+	}
+})
 
 client.login(token);
