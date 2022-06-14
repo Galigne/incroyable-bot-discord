18bb2ab68268d63e97f0877cc8e1da4841ece264
Galigne <broxarcane@gmail.com>
2022-06-15 00:16:17 +0200

    add character and display + change rules

--- commands/JDR.js
+++ commands/JDR.js
@@ -2,7 +2,7 @@ const { MessageAttachment } = require("discord.js");
 const fs = require('fs')
 const Discord = require('discord.js');
 const { info } = require("console");
-const Character = require('../classes/Character.js/index.js.js');
+const Character = require('../classes/Character.js');
 
 module.exports = {
 	name: 'jdr',
@@ -15,7 +15,7 @@ module.exports = {
                 message.reply("La commande **!JDR** n'existe pas, essayez **!JDR help**");
                 break;
             case "add":
-                addCharacter(message);
+                addCharacter(message, args[2], message.author.id);
                 break;
             case "delete":
                 delCharacter(message);
@@ -50,8 +50,16 @@ function rollD20(message) {
     message.channel.send({files: [diceGif]});
 }
 
-function addCharacter(message){
-    message.channel.send("TODO...");
+function addCharacter(message, name, creatorID){
+    var character = new Character(name, creatorID);
+    var json = JSON.stringify(character);
+    fs.writeFile(`save/${name}.json`, json, (err) => {
+        if (err)
+          message.reply("Error while saving your character", err);
+        else {
+          message.reply("Your character was created");
+        }
+    });   
 }
 
 function delCharacter(message){
@@ -59,46 +67,24 @@ function delCharacter(message){
 }
 
 function infos(message, name){
-    var character = new Character(name, "ODNZEAOIDNAOZD");
-    character.toJson();
-    message.channel.send("TODO...");
+    fs.readFile(`save/${name}.json`, 'utf8', (err, data) => {
+        if (err) {
+            message.reply("Error while saving your character", err);
+          return;
+        }
+        var character = Character.getCharacterFromSave(JSON.parse(data));
+        console.log(character);
+        var embed = character.toMessageEmbed();
+        message.channel.send(embed);
+      });
 }
 
 function rules(message){
-    var commandList = new Discord.MessageEmbed()
-        .setTitle("Livre de règles")
-        .setColor('#FFD700');
-    commandList.addField("CONSTITUTION", "Nombre de PV • Résistance aux effets de statuts (poison, paralysie…)");
-    commandList.addField("FORCE", "Maniement des armes lourdes • Bloquer ou se protéger • Actions FORCE (porter des poids, Lancer d'objets lourds…)");
-    commandList.addField("DEXTÉRITÉ", "Maniement des armes légères et à distance • Esquiver • Actions DEXTÉRITÉ (infiltration, Vol à la tire…)");
-    commandList.addField("INTELLIGENCE", "Maîtrise des LOIS • Détermine les points de LOI • Actions INTELLIGENCE (culture générale, compréhension, réflexion, calcul…)");
-    commandList.addField("VITESSE", "Détermine l’INITIATIVE et les REFLEXES • Actions VITESSE (vitesse de course et de réflexion)");
-    commandList.addField("PERCEPTION", "Précision de visée • Instinct, amélioration des 5 sens • Actions PERCEPTION (repérer un ennemi camouflé, détecter un mensonge…)");
-    commandList.addField("CHARISME", "Aisance en public, marchandage, contrôle de soi, gestion des émotions, intimidation, commandement…");
-    
-    commandList.addField("INITIATIVE", "Déterminé par la VITESSE: ordre de passage des tours (en mode combat)");
-    commandList.addField("REFLEXE", "(VITESSE + PERCEPTION) / 3 = REFLEXE (arrondit au plus proche) • Capacité à réagir rapidement à un péril inattendu");
-
-    commandList.addField("POINT D'ACTION (PA)", "Détermine les actions réalisable à chaque tour • 4 points par tour • Peut économiser 50% des points restant à la fin du tour pour le suivant");
-    commandList.addField("LOI", "Maitrise d'un pouvoir (feu, télékinésie, illusion, contrôle des animaux…) • Chaque point de LOI permet d'ajouter un pouvoir ou d'en améliorer un");
-    commandList.addField("POINTS DE VIE (PV)", "CONSTITUTION * 100 • Plus vos points de vie son bas plus vous êtes proche de la mort et des malus s'appliquent sur vos JETS");
-    commandList.addField("ARMURE", "Déterminé par vos équipements • Donne un coefficient de réduction de dégat • Ne peut dépasser 65% de réduction");
-    commandList.addField("JET", "Généralement effectué avec un dé 20 • Pour réussir un JET il faut faire moins que votre statistique correspondante • 1 est un coup critique et 20 un échec critique");
-    message.channel.send(commandList);
+    const embed = require('../embeds/ruleList.json');
+    message.channel.send({ embed, files:[{attachment:'media/book.jpg',name:'book.jpg'}] });
 }
 
 function help(message){
-    var commandList = new Discord.MessageEmbed()
-        .setTitle("Jeu De Rôle")
-        .setDescription('Voici la liste des commandes !JDR ')
-        .setColor('#FFD700')
-    commandList.addField("add <Name>", "Ajoute un nouveau personnage");
-    commandList.addField("delete <Name>", "Supprime un personnage");
-    commandList.addField("<Name>", "Affiche les informations du personnage");
-    commandList.addField("<Name> hit <PV>", "Inflige des dégats au personnage");
-    commandList.addField("<Name> heal <PV>", "Heal des HP au personnage");
-    commandList.addField("<Name> armor set <%Armure>", "Change le coefficient d'armure du personnage");
-    commandList.addField("rules", "Affiche le livre de règle");
-    commandList.addField("help", "Affiche toutes les commandes JDR");
-    message.channel.send(commandList);
+    const embed = require('../embeds/commandList.json');
+    message.channel.send({ embed, files:[{attachment:'media/logo.jpg',name:'logo.jpg'}] });
 }
